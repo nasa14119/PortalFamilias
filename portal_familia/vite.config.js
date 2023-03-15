7139df839ec14d9744d5f0c0d46bf59b8d6d8dd6
nasa14119 <nico.amaya0205@gmail.com>
2023-03-14 20:36:30 -0600

    Adding work 0.0

--- portal_familia/vite.config.js
+++ portal_familia/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/nasa14119/PortalFamilias_Redisen-o/"
+  base: "https://nasa14119.github.io/PortalFamilias_Redisen-o/"
 })
