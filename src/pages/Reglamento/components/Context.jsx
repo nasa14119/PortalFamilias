ab2db31a409b3f202b529245aec62607935da0ed
nasa14119 <nico.amaya0205@gmail.com>
2024-10-27 21:57:09 -0600

    Adding context

---
+++ src/pages/Reglamento/components/Context.jsx
@@ -0,0 +1,49 @@
+import { createContext, useContext } from "react";
+import { max_index, useFileState } from "../hooks/useFileState";
+const contextPdf = createContext(null);
+export function ContextPdf({ children }) {
+  const [file, setIndex, currentIndex] = useFileState();
+  return (
+    <contextPdf.Provider
+      value={{
+        file,
+        setIndex,
+        currentIndex,
+      }}
+    >
+      {children}
+    </contextPdf.Provider>
+  );
+}
+export const useFileContext = () => {
+  const { file } = useContext(contextPdf);
+  return file;
+};
+export const useGoNextFile = () => {
+  const { setIndex, currentIndex } = useContext(contextPdf);
+  const newValue = currentIndex + 1;
+  return () => {
+    if (newValue > max_index) {
+      setIndex(0);
+      return;
+    }
+    setIndex(newValue);
+  };
+};
+export const useGoPrevFile = () => {
+  const { setIndex, currentIndex } = useContext(contextPdf);
+  const newValue = currentIndex - 1;
+  return () => {
+    if (newValue < 0) {
+      setIndex(max_index);
+      return;
+    }
+    setIndex(newValue);
+  };
+};
+export const useSetFileTo = () => {
+  const { setIndex } = useContext(contextPdf);
+  return (newValue) => {
+    setIndex(newValue);
+  };
+};
