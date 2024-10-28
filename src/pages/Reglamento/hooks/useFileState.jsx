e6fa5859658623ead3295a138fe61dff5acb8d54
nasa14119 <nico.amaya0205@gmail.com>
2024-10-27 22:11:50 -0600

    adding file state

---
+++ src/pages/Reglamento/hooks/useFileState.jsx
@@ -0,0 +1,19 @@
+import FILES from "../FILES_2.json";
+import { useEffect, useState } from "react";
+export const max_index = FILES.length;
+export const useFileState = () => {
+  const [index, setIndex] = useState(0);
+  const [state, setState] = useState(FILES[0]);
+  const setToIndex = (param) => {
+    if (typeof index === "undefined") {
+      setIndex((prev_index) =>
+        prev_index + 1 > max_index ? 0 : prev_index + 1
+      );
+    }
+    setIndex(param);
+  };
+  useEffect(() => {
+    setState(FILES[index]);
+  }, [index]);
+  return [{ ...state, url: `Data_reglamento/${state.url}` }, setToIndex, index];
+};
